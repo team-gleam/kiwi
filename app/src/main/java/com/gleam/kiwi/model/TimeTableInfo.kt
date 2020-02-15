fc135e699692da621d26c24bbf1802468f89fa32
narupi <narupi1999@gmail.com>
2020-02-15 16:05:40 +0900

    Create Json structures

--- app/src/main/java/com/gleam/kiwi/model/TimeTableInfo.kt
+++ app/src/main/java/com/gleam/kiwi/model/TimeTableInfo.kt
@@ -1,1 +1,49 @@
 package com.gleam.kiwi.model
+
+data class TimeTableInfo(
+    val mon: Mon,
+    val tue: Tue,
+    val wed: Wed,
+    val thu: Thu,
+    val fri: Fri
+)
+
+data class Mon(
+    val `1`: String,
+    val `2`: String,
+    val `3`: String,
+    val `4`: String,
+    val `5`: String
+)
+
+data class Tue(
+    val `1`: String,
+    val `2`: String,
+    val `3`: String,
+    val `4`: String,
+    val `5`: String
+)
+
+data class Wed(
+    val `1`: String,
+    val `2`: String,
+    val `3`: String,
+    val `4`: String,
+    val `5`: String
+)
+
+data class Thu(
+    val `1`: String,
+    val `2`: String,
+    val `3`: String,
+    val `4`: String,
+    val `5`: String
+)
+
+data class Fri(
+    val `1`: String,
+    val `2`: String,
+    val `3`: String,
+    val `4`: String,
+    val `5`: String
+)
