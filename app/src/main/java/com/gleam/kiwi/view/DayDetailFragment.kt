cb714119d4b7d1e9b31b75eed6655f96e63d969c
narupi <narupi1999@gmail.com>
2020-03-18 17:07:08 +0900

    implement date filter

--- app/src/main/java/com/gleam/kiwi/view/DayDetailFragment.kt
+++ app/src/main/java/com/gleam/kiwi/view/DayDetailFragment.kt
@@ -7,28 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.gleam.kiwi.R
+import com.gleam.kiwi.viewModel.DayDetailViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DayDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DayDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+   // private val dayDetailViewModel by lazy { ViewModelProvider.NewInstanceFactory().create(DayDetailViewModel::class.java) }
+    private val dayDetailViewModel: DayDetailViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -43,23 +31,4 @@ class DayDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DayDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DayDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/gleam/kiwi/viewModel/DayDetailViewModel.kt
+++ app/src/main/java/com/gleam/kiwi/viewModel/DayDetailViewModel.kt
@@ -2,16 +2,20 @@ package com.gleam.kiwi.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.gleam.kiwi.model.Task
 import com.gleam.kiwi.model.Tasks
 import com.gleam.kiwi.net.KiwiClient
 import com.gleam.kiwi.net.KiwiService
 import com.gleam.kiwi.net.KiwiServiceInterFace
 import kotlinx.coroutines.runBlocking
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
-class DayDetailViewModel() : ViewModel() {
+class DayDetailViewModel(private val day: String) : ViewModel() {
     private val token = "placeholder"
-    var taskList = MutableLiveData<Tasks>()
+    var taskList = MutableLiveData<List<Task>>()
 
     init {
         loadTaskList()
@@ -23,15 +27,18 @@ class DayDetailViewModel() : ViewModel() {
                 val client = KiwiClient(KiwiService().create(KiwiServiceInterFace::class.java))
                 val tasks: Tasks? = client.getTasks(token)
                 if (tasks != null){
-                    //TODO: Pick task from tasks
-                    taskList.postValue(tasks)
+                    taskList.postValue(getDayTasks(tasks, day))
                 }else {
-
                 }
             } catch (e: Exception){
                 e.stackTrace
             }
         }
+    }
+
+   // 20XX-10-10
+    private fun getDayTasks(tasks: Tasks, day: String): List<Task>{
+        return tasks.tasks.filter{ it.date == day }
     }
 
 }
